33b9d4f03c8c1f8e13279ed0ae33f6cb9fdeb7de
yangdehong <405104787@qq.com>
2020-12-11 16:12:40 +0800

    init

---
+++ self_mybatis/src/test/java/com/ydh/redsheep/self_mybatis/SelfMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydh.redsheep.self_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
