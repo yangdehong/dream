7e5dddfc2057929f8f846bade1af50d3cd199ecc
yangdehong <405104787@qq.com>
2020-12-17 13:39:26 +0800

    spring-demo

---
+++ self_springmvc/src/main/java/com/ydh/redsheep/self_springmvc/SelfSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.ydh.redsheep.self_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfSpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfSpringmvcApplication.class, args);
+    }
+
+}
---
+++ self_springmvc/src/test/java/com/ydh/redsheep/self_springmvc/SelfSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydh.redsheep.self_springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfSpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
